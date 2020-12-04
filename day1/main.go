97c1146e785283a51986e2006d0e2f34a82154f3
Marvin Zwolsman <mzwolsman@bol.com>
2020-12-04 12:27:42 +0100

    Day 1

---
+++ day1/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	file, err := os.Open("/Users/mzwolsman/Developer/rts-ng/rts-ng/go-aoc/day1/input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	var numbers []int
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		number, err := strconv.Atoi(line)
+		if err != nil {
+			log.Fatal(err)
+		}
+		numbers = append(numbers, number)
+	}
+
+	for i := 0; i < len(numbers); i++ {
+		for j := 0; j < len(numbers); j++ {
+			if j == i {
+				continue
+			}
+			for k := 0; k < len(numbers); k++ {
+				if k == j {
+					continue
+				}
+
+				a := numbers[i]
+				b := numbers[j]
+				c := numbers[k]
+
+				if a+b+c == 2020 {
+					fmt.Println(a * b * c)
+					return
+				}
+			}
+		}
+	}
+}
