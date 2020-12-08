ab7583ec6ee5aab543f29d725cd529053a5086a7
Marvin Zwolsman <mzwolsman@bol.com>
2020-12-08 09:52:49 +0100

    feat(day-8): part 2

--- day8/main.go
+++ day8/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"io/ioutil"
 	"log"
 	"strconv"
@@ -14,13 +15,51 @@ func main() {
 	}
 
 	instructions := strings.Split(string(data), "\n")
+	for i := 0; i < len(instructions); i++ {
+		println("try", i+1)
+		newInstructions := mutateInstructions(instructions, i)
+		acc, err := runProgram(newInstructions)
+		if err == nil {
+			println(acc)
+			break
+		}
+	}
+}
+
+var replacer = strings.NewReplacer("jmp", "nop", "nop", "jmp")
+
+func mutateInstructions(instructions []string, offset int) []string {
+	mutatedInstructions := make([]string, len(instructions))
+	copy(mutatedInstructions, instructions)
+
+	for i := len(mutatedInstructions) - 1; i != -1; i-- {
+		instruction := mutatedInstructions[i]
+
+		if instruction[:3] == "jmp" {
+			offset--
+		}
+		if instruction[:3] == "nop" {
+			offset--
+		}
+
+		if offset == -1 { //do mutation
+			mutatedInstructions[i] = replacer.Replace(instruction)
+			return mutatedInstructions
+		}
+	}
+	panic("NOTHING HAS BEEN REPLACED!")
+}
+
+func runProgram(instructions []string) (acc int, err error) {
 	ptr := 0
-	acc := 0
 	history := make(map[int]bool)
 	for true {
-		println(ptr, acc, instructions[ptr])
+		//println(ptr, acc, instructions[ptr])
 		if _, ok := history[ptr]; ok {
-			break
+			return -1, errors.New("infinite loop")
+		}
+		if ptr > len(instructions)-1 {
+			return -1, errors.New("index out of range [626] with length 626")
 		}
 		history[ptr] = true
 		instruction := instructions[ptr][:3]
@@ -47,10 +86,11 @@ func main() {
 			break
 		}
 
-		if ptr > len(instructions) {
+		if ptr > len(instructions)-1 {
+			println(strings.Join(instructions, "\n"))
 			println("no instructions left")
 			break
 		}
 	}
-	println(acc)
+	return acc, nil
 }
