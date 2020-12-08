9436353cb2b6391b9164732f8be13bb5c4ef95e9
Marvin Zwolsman <mzwolsman@bol.com>
2020-12-08 09:55:23 +0100

    refactor(day-8): add both parts into the program

--- day8/main.go
+++ day8/main.go
@@ -15,8 +15,15 @@ func main() {
 	}
 
 	instructions := strings.Split(string(data), "\n")
+	part1(instructions)
+	part2(instructions)
+}
+func part1(instructions []string) {
+	acc, _ := runProgram(instructions)
+	println(acc)
+}
+func part2(instructions []string) {
 	for i := 0; i < len(instructions); i++ {
-		println("try", i+1)
 		newInstructions := mutateInstructions(instructions, i)
 		acc, err := runProgram(newInstructions)
 		if err == nil {
@@ -56,10 +63,10 @@ func runProgram(instructions []string) (acc int, err error) {
 	for true {
 		//println(ptr, acc, instructions[ptr])
 		if _, ok := history[ptr]; ok {
-			return -1, errors.New("infinite loop")
+			return acc, errors.New("infinite loop")
 		}
 		if ptr > len(instructions)-1 {
-			return -1, errors.New("index out of range [626] with length 626")
+			return acc, errors.New("index out of range [626] with length 626")
 		}
 		history[ptr] = true
 		instruction := instructions[ptr][:3]
@@ -87,8 +94,6 @@ func runProgram(instructions []string) (acc int, err error) {
 		}
 
 		if ptr > len(instructions)-1 {
-			println(strings.Join(instructions, "\n"))
-			println("no instructions left")
 			break
 		}
 	}
