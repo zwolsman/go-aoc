d303c6aeb687a306ab4ba17a91b5b0300d441a4b
Marvin Zwolsman <mzwolsman@bol.com>
2020-12-11 11:46:07 +0100

    feat(2019-1): part 2

--- 2019/day1/main.go
+++ 2019/day1/main.go
@@ -27,6 +27,7 @@ func main() {
 	}
 
 	part1(modules)
+	part2(modules)
 }
 
 func part1(modules []int) {
@@ -37,4 +38,25 @@ func part1(modules []int) {
 	}
 
 	println("The sum of the fuel requirements for all of the modules on the spacecraft", sum)
+}
+
+func part2(modules []int) {
+	sum := 0
+
+	var requiredFuel func(int) int
+
+	requiredFuel = func(module int) int {
+		fuel := int(math.Floor(float64(module)/3.0) - 2)
+		if fuel <= 0 {
+			return 0
+		} else {
+			return fuel + requiredFuel(fuel)
+		}
+	}
+
+	for _, module := range modules {
+		sum += requiredFuel(module)
+	}
+
+	println("The sum of the fuel requirements for all of the modules on the spacecraft when also taking into account the mass of the added fuel", sum)
 }
