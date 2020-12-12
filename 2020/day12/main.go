0abce4ac98f0ac4bc8f3266cf8cf3a11d9947156
Marvin Zwolsman <mzwolsman@bol.com>
2020-12-12 11:38:55 +0100

    feat(2020-12): part 2

--- 2020/day12/main.go
+++ 2020/day12/main.go
@@ -19,6 +19,14 @@ func (v1 Vector) Mult(amt float64) Vector {
 func (v1 Vector) Add(v2 Vector) Vector {
 	return Vector{v1.x + v2.x, v1.y + v2.y}
 }
+func (v1 Vector) Min(v2 Vector) Vector {
+	return Vector{v1.x - v2.x, v1.y - v2.y}
+}
+func (v Vector) Rotate(radians float64) Vector {
+	ca := math.Cos(radians)
+	sa := math.Sin(radians)
+	return Vector{ca*v.x - sa*v.y, sa*v.x + ca*v.y}
+}
 func (v Vector) Length() float64 {
 	return math.Abs(v.x) + math.Abs(v.y)
 }
@@ -36,7 +44,11 @@ func main() {
 	}
 
 	instructions := strings.Split(string(data), "\n")
+	println("*** part 1 ***")
 	part1(instructions)
+
+	println("*** part 2 ***")
+	part2(instructions)
 }
 
 func part1(instructions []string) {
@@ -83,5 +95,52 @@ func part1(instructions []string) {
 		println("instruction", instruction, "position:", position.String())
 	}
 
-	fmt.Printf("%v, %.f", position, position.Length())
+	fmt.Printf("%v, %.f\n\n", position, position.Length())
+}
+
+func part2(instructions []string) {
+	waypoint := Vector{10, -1}
+	ship := Vector{}
+
+	for _, instruction := range instructions {
+		num, err := strconv.Atoi(instruction[1:])
+		if err != nil {
+			log.Fatal(err)
+		}
+		arg := float64(num)
+
+		var v Vector
+		switch instruction[0] {
+		case 'N':
+			v = Vector{y: -arg}
+			break
+		case 'S':
+			v = Vector{y: arg}
+			break
+		case 'E':
+			v = Vector{x: arg}
+			break
+		case 'W':
+			v = Vector{x: -arg}
+			break
+		case 'F':
+			v = waypoint.Min(ship).Mult(arg)
+			ship = ship.Add(v)
+			break
+		case 'R':
+			radians := arg * (math.Pi / 180)
+			line := waypoint.Min(ship)
+			waypoint = ship.Add(line.Rotate(radians))
+			break
+		case 'L':
+			radians := arg * (math.Pi / 180)
+			line := waypoint.Min(ship)
+			waypoint = ship.Add(line.Rotate(-radians))
+			break
+		}
+
+		waypoint = waypoint.Add(v)
+		println(instruction, "-> ship", ship.String(), "waypoint", waypoint.String())
+	}
+	fmt.Printf("%v, %.f\n\n", ship, ship.Length())
 }
