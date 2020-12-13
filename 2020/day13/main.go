3126494e57f16b54183915952dca96ceff63ed1f
Marvin Zwolsman <mzwolsman@bol.com>
2020-12-13 11:34:09 +0100

    feat(2020-13): part 2

--- 2020/day13/main.go
+++ 2020/day13/main.go
@@ -17,6 +17,15 @@ func main() {
 
 	lines := strings.Split(string(data), "\n")
 
+	println("*** part 1 ***")
+	part1(lines)
+	println()
+
+	println("*** part 2***")
+	part2(lines)
+}
+
+func part1(lines []string) {
 	minutes, err := strconv.Atoi(lines[0])
 	if err != nil {
 		log.Fatal(err)
@@ -31,14 +40,7 @@ func main() {
 		buses = append(buses, b)
 	}
 
-	fmt.Printf("%v, %v\n", minutes, buses)
-
-	println("*** part 1 ***")
-	part1(minutes, buses)
-
-}
-
-func part1(minutes int, buses []int) {
+	//fmt.Printf("%v, %v\n", minutes, buses)
 
 	longestWait, id := math.MaxInt64, -1
 	for _, bus := range buses {
@@ -48,7 +50,51 @@ func part1(minutes int, buses []int) {
 			longestWait = wait
 			id = wait * bus
 		}
-		fmt.Printf("bus %d, next dept: %d, wait: %d, answer: %d\n", bus, next, wait, (next-minutes)*bus)
+		//fmt.Printf("bus %d, next dept: %d, wait: %d, answer: %d\n", bus, next, wait, (next-minutes)*bus)
 	}
 	fmt.Printf("The ID of the earliest bus you can take to the airport multiplied by the number of minutes you'll need to wait for that bus is %d\n", id)
+}
+
+func part2(lines []string) {
+	var buses []int
+
+	for _, b := range strings.Split(lines[1], ",") {
+		var n int
+		if b == "x" {
+			n = 1
+		} else {
+			n, _ = strconv.Atoi(b)
+		}
+
+		buses = append(buses, n)
+	}
+
+	interval, departure := 1, 1
+
+	for i, bus := range buses {
+		slice := buses[:i+1]
+		for {
+			if validate(slice, departure) {
+				interval *= bus
+				break
+			} else {
+				departure += interval
+			}
+		}
+	}
+
+	println("The earliest timestamp such that all of the listed bus IDs depart at offsets matching their positions in the list is", departure)
+}
+
+func validate(buses []int, departure int) bool {
+	for i, bus := range buses {
+		if !isValidDeparture(i, bus, departure) {
+			return false
+		}
+	}
+	return true
+}
+
+func isValidDeparture(index, busId, departure int) bool {
+	return (departure+index)%busId == 0
 }
