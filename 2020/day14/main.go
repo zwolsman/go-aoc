2b9e1ef007cc82b98bf940f01199a886d3c3b436
Marvin Zwolsman <mzwolsman@bol.com>
2020-12-14 22:04:16 +0100

    feat(2020-14): part 2

--- 2020/day14/main.go
+++ 2020/day14/main.go
@@ -17,6 +17,7 @@ const (
 
 func main() {
 	part1()
+	part2()
 }
 
 func part1() {
@@ -50,6 +51,88 @@ func part1() {
 		sum += v
 	}
 	println(sum)
+}
+
+func part2() {
+	file, err := os.Open("./2020/day14/input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	scanner := bufio.NewScanner(file)
+	maskRegexp, memRegexp := setupRegexp()
+
+	mem := make(map[int]int)
+	var mask []bit
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if maskRegexp.MatchString(line) {
+			match := maskRegexp.FindAllStringSubmatch(line, 1)
+			mask = readMask(match[0][1])
+		}
+		if memRegexp.MatchString(line) {
+			match := memRegexp.FindAllStringSubmatch(line, 2)
+			addr, value := toInt(match[0][1]), toInt(match[0][2])
+			for _, address := range calculateAddresses(mask, addr) {
+				mem[fromBits(address)] = value
+			}
+		}
+	}
+
+	sum := 0
+	for _, v := range mem {
+		sum += v
+	}
+	println(sum)
+}
+
+func calculateAddresses(mask []bit, addr int) [][]bit {
+	bits := readBits(bitSize, addr)
+	actual := make([]bit, bitSize)
+	for i, m := range mask {
+		var bit bit
+		if m == unset {
+			bit = bits[i]
+		} else {
+			bit = m
+		}
+		actual[i] = bit
+	}
+
+	return getAddresses(actual)
+}
+
+func getAddresses(mask []bit) (result [][]bit) {
+	isComplete := func() bool {
+		for _, b := range mask {
+			if b == floating {
+				return false
+			}
+		}
+		return true
+	}
+
+	if isComplete() {
+		result = append(result, mask)
+		return
+	}
+
+	for i, b := range mask {
+		if b == floating {
+			result = append(result, getAddresses(copyAndSet(mask, i, 0))...)
+			result = append(result, getAddresses(copyAndSet(mask, i, 1))...)
+			return
+		}
+	}
+
+	return
+}
+
+func copyAndSet(src []bit, i int, v bit) []bit {
+	output := make([]bit, len(src))
+	copy(output, src)
+	output[i] = v
+	return output
 }
 
 func applyMask(mask, arr []bit) []bit {
@@ -92,7 +175,7 @@ func setupRegexp() (*regexp.Regexp, *regexp.Regexp) {
 type bit int
 
 const (
-	ignore bit = iota - 1
+	floating bit = iota - 1
 	unset
 	set
 )
@@ -103,7 +186,7 @@ func readMask(mask string) []bit {
 		var b bit
 		switch m {
 		case 'X':
-			b = ignore
+			b = floating
 		case '0', '1':
 			b = bit(m - '0')
 		}
