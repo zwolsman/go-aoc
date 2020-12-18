c257f4133fbac11a05236873f7025032a8d1186b
Marvin Zwolsman <mzwolsman@bol.com>
2020-12-18 15:34:20 +0100

    feat(2019-10): part 2

--- 2019/day10/main.go
+++ 2019/day10/main.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"math"
+	"sort"
 	"strings"
 )
 
@@ -25,11 +26,12 @@ func main() {
 		}
 	}
 
-	part1(asteroids)
+	laser := part1(asteroids)
+	part2(laser, asteroids)
 }
 
-func part1(asteroids []Vector) {
-	best, pos := 0, Vector{}
+func part1(asteroids []Vector) (pos Vector) {
+	best := 0
 	for i := 0; i < len(asteroids); i++ {
 		base := asteroids[i]
 		angles := make(map[float64]bool)
@@ -49,6 +51,53 @@ func part1(asteroids []Vector) {
 		}
 	}
 	fmt.Printf("Best position is %v where we can see %d other astroids in direct line of sight.\n", pos, best)
+	return
+}
+
+func part2(laser Vector, asteroids VectorArray) {
+
+	asteroidMap := make(map[float64]VectorArray)
+	var angles []float64
+	for _, asteroid := range asteroids {
+		if asteroid == laser {
+			continue
+		}
+		dist := laser.min(asteroid)
+		angle := dist.Angle()
+		asteroidMap[angle] = append(asteroidMap[angle], dist)
+	}
+
+	// sort by distance
+	for angle, v := range asteroidMap {
+		angles = append(angles, angle)
+		sort.Sort(v)
+
+		asteroidMap[angle] = v
+	}
+	sort.Sort(sort.Float64Slice(angles))
+	curr := sort.SearchFloat64s(angles, Vector{x: 0, y: 1}.Angle())
+
+	for i := 0; i < 200; i++ {
+		var acquireTarget func() (float64, VectorArray)
+
+		acquireTarget = func() (float64, VectorArray) {
+			angle := angles[curr%len(angles)]
+			fmt.Printf("using angle %v\n", angle)
+			targets, ok := asteroidMap[angle]
+			curr++
+			if !ok {
+				log.Fatal("getting asteroid is not ok.")
+			}
+			if len(targets) == 0 { //no more asteroids left..
+				return acquireTarget()
+			}
+			return angle, targets
+		}
+
+		angle, targets := acquireTarget()
+		fmt.Printf("Round %d -> Shooting from %v to %v; all targets: %v\n\n", i+1, laser, laser.min(targets[0]), targets)
+		asteroidMap[angle] = targets[1:]
+	}
 }
 
 type Vector struct {
@@ -65,6 +114,16 @@ func (v Vector) min(o Vector) Vector {
 func (v Vector) Angle() float64 {
 	return math.Atan2(v.y, v.x)
 }
+
+func (p Vector) Length() int {
+	return int(math.Abs(p.x) + math.Abs(p.y))
+}
+
+type VectorArray []Vector
+
+func (p VectorArray) Len() int           { return len(p) }
+func (p VectorArray) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+func (p VectorArray) Less(i, j int) bool { return p[i].Length() < p[j].Length() }
 
 func (v Vector) String() string {
 	return fmt.Sprintf("Vector(%.f, %.f)", v.x, v.y)
