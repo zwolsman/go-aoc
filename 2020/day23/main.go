5d62be1062a953e5da70423a9015fc5b30dd64f3
Marvin Zwolsman <mzwolsman@bol.com>
2020-12-26 14:33:03 +0100

    feat(2020-23): part 2

--- 2020/day23/main.go
+++ 2020/day23/main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"container/ring"
-	"fmt"
-	"golang.org/x/tools/container/intsets"
 	"io/ioutil"
 	"log"
 	"strconv"
@@ -11,120 +8,84 @@ import (
 )
 
 func main() {
-
 	part1()
+	part2()
 }
 
 func part1() {
-	cups, min, max := readCups()
+	cups := readCups()
 
-	cups = playCups(cups, 100, min, max)
-	printCups(cups)
-}
-
-func playCups(cups *ring.Ring, rounds, min, max int) *ring.Ring {
-	for i := 0; i < 100; i++ {
-		cup := cups.Value.(int)
-		//fmt.Printf("-- move %d --\n", i+1)
-		//print("all cups: ")
-		//printCupsWithHighlight(cups, cup)
-		pickUp := cups.Unlink(3)
-
-		//print("pickup: ")
-		//printCups(pickUp)
-		target := cup - 1
-		for {
-			if target < min {
-				target = max
-			}
-			if !containsCup(target, pickUp) {
-				break
-			}
-			//println("target", target, "is already in the pickups. substracting one")
-			target--
-
-		}
-
-		//println("target cup -> ", target)
-		for {
-			if cups.Value.(int) == target {
-				break
-			}
-			cups = cups.Next()
-		}
-
-		cups.Link(pickUp)
-		for {
-			if cups.Value.(int) == cup {
-				break
-			}
-			cups = cups.Next()
-		}
-		cups = cups.Next()
-	}
-
-	//println("-- final --")
-	return cups
-}
-
-func containsCup(cup int, cups *ring.Ring) bool {
-	if cups.Value.(int) == cup {
-		return true
-	}
-	for p := cups.Next(); p != cups; p = p.Next() {
-		if p.Value.(int) == cup {
-			return true
-		}
-	}
-	return false
-}
-
-func printCups(cups *ring.Ring) {
-	print(cups.Value.(int), " ")
-	for p := cups.Next(); p != cups; p = p.Next() {
-		print(p.Value.(int), " ")
+	cups = playCups(cups, 100)
+	for _, cup := range cups {
+		print(cup+1, " ")
 	}
 	println()
 }
 
-func printCupsWithHighlight(cups *ring.Ring, highlight int) {
-	printFn := func(cup int) {
-		format := "%d "
-		if cup == highlight {
-			format = "(%d) "
-		}
+func part2() {
+	initial := readCups()
+	cups := make([]int, 1_000_000)
 
-		fmt.Printf(format, cup)
+	copy(cups, initial)
+
+	for i := len(initial); i < len(cups); i++ {
+		cups[i] = i
 	}
-	printFn(cups.Value.(int))
-	for p := cups.Next(); p != cups; p = p.Next() {
-		printFn(p.Value.(int))
-	}
-	println()
+	cups = playCups(cups, 10_000_000)
+
+	c1 := cups[0]
+	c2 := cups[c1]
+	println((c1 + 1) * (c2 + 1))
 }
 
-func readCups() (*ring.Ring, int, int) {
+func playCups(cups []int, rounds int) []int {
+
+	N := len(cups)
+	next := make([]int, N)
+	for i := 0; i < len(next); i++ {
+		next[cups[i]] = cups[mod(i+1, N)]
+	}
+
+	current := cups[0]
+	for i := 0; i < rounds; i++ {
+		c1 := next[current]
+		c2 := next[c1]
+		c3 := next[c2]
+
+		next[current] = next[c3]
+
+		dest := mod(current-1, N)
+		for c1 == dest || c2 == dest || c3 == dest {
+			dest = mod(dest-1, N)
+		}
+
+		next[c3] = next[dest]
+		next[dest] = c1
+
+		current = next[current]
+	}
+
+	return next
+}
+
+func mod(a, b int) int {
+	return (a%b + b) % b
+}
+
+func readCups() []int {
 	data, err := ioutil.ReadFile("./2020/day23/input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	r := ring.New(len(data))
-	min, max := intsets.MaxInt, 0
+	var cups []int
 	for _, str := range strings.Split(string(data), "") {
 		cup, err := strconv.Atoi(str)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		if cup > max {
-			max = cup
-		}
-		if cup < min {
-			min = cup
-		}
-		r.Value = cup
-		r = r.Next()
+		cups = append(cups, cup-1)
 	}
-	return r, min, max
+	return cups
 }
