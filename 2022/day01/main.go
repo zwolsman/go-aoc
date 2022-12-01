e4c313980bb95322103900ed82354c9fb30ff5f0
Marvin Zwolsman <mzwolsman@bol.com>
2022-12-01 09:58:03 +0100

    task: 2022 day 1

--- 2022/day01/main.go
+++ 2022/day01/main.go
@@ -5,17 +5,35 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
+	"strconv"
 )
 
 func main() {
-	file, err := os.Open("./2022/day1/input.txt")
+	file, err := os.Open("./2022/day01/input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	scanner := bufio.NewScanner(file)
 
+	var sum, max int
+	var elves []int
+
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Println(line)
+		n, _ := strconv.Atoi(line)
+		if line == "" {
+			elves = append(elves, sum)
+			sum = 0
+		} else {
+			sum += n
+			if sum > max {
+				max = sum
+			}
+		}
 	}
+	sort.Ints(elves)
+	fmt.Println(elves[len(elves)-1])
+
+	fmt.Println(elves[len(elves)-1] + elves[len(elves)-2] + elves[len(elves)-3])
 }
