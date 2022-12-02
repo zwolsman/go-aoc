c4b9780941c4246069601ad04b262c520cfe4533
Marvin Zwolsman <mzwolsman@bol.com>
2022-12-02 10:44:13 +0100

    refactor: 2022 day 2

--- 2022/day02/main.go
+++ 2022/day02/main.go
@@ -9,6 +9,12 @@ import (
 //go:embed input.txt
 var in []byte
 
+const (
+	WIN  = 0
+	DRAW = 1
+	LOSS = 2
+)
+
 func main() {
 	fmt.Println(part1(in))
 	fmt.Println(part2(in))
@@ -18,24 +24,10 @@ func part1(in []byte) any {
 	lines := strings.Split(string(in), "\n")
 	var score int
 	for i := 0; i < len(lines); i++ {
-		opponent := lines[i][0] - 'A'
-		you := lines[i][2] - 'X'
+		opponent := int(lines[i][0] - 'A')
+		you := int(lines[i][2] - 'X')
 
-		score += int(you + 1)
-		// Rock defeats Scissors, Scissors defeats Paper, and Paper defeats Rock.
-		// rock == 0
-		// paper = 1
-		// scisors == 2
-		switch {
-		case you == 0 && opponent == 2:
-			score += 6
-		case you == 2 && opponent == 1:
-			score += 6
-		case you == 1 && opponent == 0:
-			score += 6
-		case you == opponent:
-			score += 3
-		}
+		score += calculateScore(opponent, you)
 	}
 	return score
 }
@@ -45,46 +37,33 @@ func part2(in []byte) any {
 	var score int
 	for i := 0; i < len(lines); i++ {
 		opponent := int(lines[i][0] - 'A')
-
 		outcome := lines[i][2] - 'X'
+
 		var you int
+
 		switch outcome {
-		case 0: // lose
-			switch opponent {
-			case 0:
-				you = 2
-			case 1:
-				you = 0
-			case 2:
-				you = 1
-			}
-		case 1: // draw
-			you = int(opponent)
-		case 2: // win
-			switch opponent {
-			case 0:
-				you = 1
-			case 1:
-				you = 2
-			case 2:
-				you = 0
-			}
+		case LOSS:
+			you = (opponent + 1) % 3
+		case DRAW:
+			you = opponent
+		case WIN:
+			you = (opponent + 2) % 3
 		}
-		score += int(you + 1)
-		// Rock defeats Scissors, Scissors defeats Paper, and Paper defeats Rock.
-		// rock == 0
-		// paper = 1
-		// scisors == 2
-		switch {
-		case you == 0 && opponent == 2:
-			score += 6
-		case you == 2 && opponent == 1:
-			score += 6
-		case you == 1 && opponent == 0:
-			score += 6
-		case you == opponent:
-			score += 3
-		}
+		score += calculateScore(opponent, you)
 	}
 	return score
+}
+
+func calculateScore(opponent, you int) (score int) {
+	score += you + 1
+
+	// Draw
+	if opponent == you {
+		score += 3
+	}
+
+	if you == (opponent+1)%3 {
+		score += 6
+	}
+	return
 }
--- 2022/day02/main_test.go
+++ 2022/day02/main_test.go
@@ -7,18 +7,25 @@ import (
 )
 
 func Test_part1(t *testing.T) {
-	in, _ := os.ReadFile("input_test.txt")
 	tests := []struct {
 		name string
+		file string
 		want any
 	}{
 		{
 			name: "example",
+			file: "input_test.txt",
 			want: 15,
+		},
+		{
+			name: "answer",
+			file: "input.txt",
+			want: 12740,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			in, _ := os.ReadFile(tt.file)
 			if got := part1(in); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("part1() = %v, want %v", got, tt.want)
 			}
@@ -27,18 +34,25 @@ func Test_part1(t *testing.T) {
 }
 
 func Test_part2(t *testing.T) {
-	in, _ := os.ReadFile("input_test.txt")
 	tests := []struct {
 		name string
+		file string
 		want any
 	}{
 		{
 			name: "example",
+			file: "input_test.txt",
 			want: 12,
+		},
+		{
+			name: "answer",
+			file: "input.txt",
+			want: 11980,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			in, _ := os.ReadFile(tt.file)
 			if got := part2(in); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("part2() = %v, want %v", got, tt.want)
 			}
